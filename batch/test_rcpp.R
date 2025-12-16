477711b7dd7230bdf07cbdcd60976ab306d8da4e
Edward Kuo <ekuo@actuarialperspective.com>
2025-12-16 16:43:53 -0500

    Delete unnecessary file

--- batch/test_rcpp.R
+++
@@ -1,7 +0,0 @@
-library(Rgogo)
-library(MortTables.CA)
-q <- LookUp(Mort.CIA8692L.M, lookUpKey = list(IssAge = 40))
-i <- 0.05
-n <- 25
-microbenchmark::microbenchmark(CalcNSP.TermLife(q, i, n), times = 1000)
-microbenchmark::microbenchmark(CalcNSP.WholeLife(q, i), times = 1000)
